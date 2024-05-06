b59fbc9f29029f1028769e7ded066b678b7eea68
bankaipain <86176027+bankaipain@users.noreply.github.com>
2024-05-06 15:28:37 +0000

    Second Commit

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ def predict():
     return render_template('index.html', prediction=prediction, email= email)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
